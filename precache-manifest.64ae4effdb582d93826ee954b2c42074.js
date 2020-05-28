a3462d800f670b4c1cb9237bc9a1ffe92f56c71b
AndreaS <solfa_90@hotmail.it>
2020-05-28 18:23:52 +0200

    Updates

---
+++ precache-manifest.64ae4effdb582d93826ee954b2c42074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a71cb6bbcb2c7de15e8fa886eca3dc",
+    "url": "/nellyMeteoApp/index.html"
+  },
+  {
+    "revision": "f51d104dace137a3aae2",
+    "url": "/nellyMeteoApp/static/css/main.653878cc.chunk.css"
+  },
+  {
+    "revision": "7bc528e0583eaa956293",
+    "url": "/nellyMeteoApp/static/js/2.9ab457d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nellyMeteoApp/static/js/2.9ab457d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51d104dace137a3aae2",
+    "url": "/nellyMeteoApp/static/js/main.74f991e4.chunk.js"
+  },
+  {
+    "revision": "133e896a51e901512143",
+    "url": "/nellyMeteoApp/static/js/runtime-main.e256e6f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a96e886c3588d3de727a385130903e9e.js"
+  "/nellyMeteoApp/precache-manifest.64ae4effdb582d93826ee954b2c42074.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nellyMeteoApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
